82b01848d202d49716e83548948fbdb3bf314581
Sneekyboots <sneekyboots122@gmail.com>
2023-11-04 04:53:35 +0530

    crawl single page with error

--- crawl.js
+++ crawl.js
@@ -1,6 +1,27 @@
 const { link } = require('fs')
 const {JSDOM}=require('jsdom')
 
+ async function crawlpage(currentUrl){
+console.log(`actively crawling :${currentUrl}`)
+try{
+const resp= await fetch(currentUrl)
+if(resp.status>399){
+    console.log(`error in fetch with status code:${resp.status} on page ${currentUrl}`)
+    return
+}
+
+const contentType=resp.headers.get("content-type")
+if(!contentType.includes("text/html")){
+    console.log(`non html response, content type: ${contentType},on page ${currentUrl}`)
+    return 
+}
+console.log( await resp.text())}
+catch(err){
+    console.log(`error in fetch :${err.message},on page:${currentUrl}`)
+    return
+}
+}
+
 function normalizeURL(urlString){
     const urlObj= new URL(urlString)
     const hostpath=`${urlObj.hostname}${urlObj.pathname}`
@@ -38,6 +59,7 @@ function geturlsfromHtml(htmlBody,baseURL){
 
 module.exports={
     normalizeURL,
-    geturlsfromHtml
+    geturlsfromHtml,
+    crawlpage
    
 }
